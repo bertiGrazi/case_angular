b7a8901e19bfe1ece615e979bdaab520ef1e79c3
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-06-04 17:13:55 -0300

    feat: start create form

--- case_angular/src/app/pages/register/register.component.ts
+++ case_angular/src/app/pages/register/register.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { DefaultLayoutComponent } from '../../components/default-layout/default-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    DefaultLayoutComponent
+    DefaultLayoutComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
 
+  constructor() {
+    this.registerForm = new FormGroup({
+      nameItem: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(50),
+        Validators.pattern('^[a-zA-Z]+$')
+      ]) 
+    })
+  }
 }
